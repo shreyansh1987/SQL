830ccf38db3dba0b1b033145ac1297935c4531a7
Shreyansh Kumar <Shreyansh Kumar>
2016-08-04 15:17:54 +0530

    shreyansh_datafetch_queries.sql

--- queries/shreyansh_datafetch_queries.sql
+++ queries/shreyansh_datafetch_queries.sql
@@ -1,1 +1,3 @@
-SELECT distinct 
+SELECT `MyCustomer`.`ms_fname` , `MyCustomer`.`ms_lname` , `MyProducts`.`mp_prod_code` , `MyProducts`.`mp_prod_type`
+FROM `MyCustomer` LEFT JOIN `MyProducts` ON `MyCustomer`.`ms_product_id_fk` = `MyProducts`.`mp_prod_id_pk` WHERE `MyProducts`
+.`mp_prod_functional` = 1 AND `MyProducts`.`mp_prod_last_updated` <= '2016-04-09 00:24:30';
